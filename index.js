9e2438d3ae48ec4b4b6cea7245177dd9f3f6b4f8
Adam <adamchasetaylor@users.noreply.github.com>
2020-09-11 22:04:19 -0500

    Update index.js
    cleaned up sloppy code

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-addEventListener('fetch', event => {
-  event.respondWith(forwardReport(event.request))
-})
+addEventListener('fetch', (event) => {
+  event.respondWith(forwardReport(event.request));
+});
 
 async function forwardReport(req) {
+  const url = new URL(req.url);
+  let path = url.pathname;
+  let request = false;
 
-  const url = new URL(req.url)
-  const path = url.pathname
-  const search = url.search
-  const address = `${PROTOCOL}://${ORIGIN}${path}${search}`
+  if (path === '/' || path === '') {
+    path = '/index.html';
+  }
 
-  let response = false
+  if (path === '/favicon.ico') {
+    return new Response('', {
+      status: 302,
+      headers: { Location: `https://www.twilio.com/favicon.ico` },
+    });
+  }
+
+  const { search } = url;
+  const address = `${PROTOCOL}://${ORIGIN}${path}${search}`;
+
+  console.log(address);
+
+  let response = false;
 
   const cacheKey = new Request(url, {
     method: req.method,
-    headers: req.headers
-  })
+    headers: req.headers,
+  });
 
-  //Work Around for Missing FavIcon
-  if ( path == '/favicon.ico' ){
-    return new Response('', {
-      status: 302,
-      headers: { "Location": `https://www.twilio.com/favicon.ico` }
-    })   
-  }
-
-  let cache = caches.default
+  const cache = caches.default;
 
   // try to find the cache key in the cache
-  response = await cache.match(cacheKey)
+  response = await cache.match(cacheKey);
 
   // otherwise, fetch from origin, add to cache
   if (!response) {
+    request = new Request(address, req);
+    request.headers.set('Origin', new URL(address).origin);
 
-    request = new Request(address, req)
-    request.headers.set('Origin', new URL(address).origin)
+    console.log(new Map(request.headers));
 
-   
- console.log(new Map(request.headers))
-
-
-    response = await fetch(request, { cf: { cacheTtl: 300 }  })
-    cache.put(cacheKey, response.clone())
+    response = await fetch(request, { cf: { cacheTtl: 300 } });
+    cache.put(cacheKey, response.clone());
   }
- 
+
   // Check response is html content
-  if (
-    !response.headers.get("content-type") ||
-    !response.headers.get("content-type").includes("text/html")
-  ) {
+  if (!response.headers.get('content-type') || !response.headers.get('content-type').includes('text/html')) {
     return response;
   }
 
@@ -55,13 +55,13 @@ async function forwardReport(req) {
   const text = await response.text();
 
   // Modify it.
-  var re = new RegExp(ORIGIN,"g");
+  const re = new RegExp(ORIGIN, 'g');
   const modified = text.replace(re, url.host);
-  
+
   // Send it back, With Content-Type Set
   return new Response(modified, {
     status: response.status,
     statusText: response.statusText,
-    headers: { "Content-Type": "text/html" }
-  })
+    headers: { 'Content-Type': 'text/html' },
+  });
 }
